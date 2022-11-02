fe97d921c8e049aff658edf6e4b983dd7b4db506
emmyvera <emmyvera01@gmail.com>
2022-11-02 21:33:47 +0100

    Added Flatmate Bill App

---
+++ Flatmate Bill App/app.py
@@ -0,0 +1,23 @@
+class Bill:
+    
+    def __init__(self, amount, period):
+        self.amount = amount
+        self.period = period
+        
+
+    pass
+
+class Flatmate:
+    
+    def __init__(self, name, days_in_house):
+        self.name = name
+        self.days_in_house = days_in_house
+        pass
+
+    pass
+
+class PDFReport:
+
+    def __init__(self, filename):
+        self.filename = filename
+    
